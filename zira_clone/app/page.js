6930045ec706d4afbd6fccc0af20d9bd4652a9f0
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-11-29 12:06:01 +0530

    .babelrc

--- zira_clone/app/page.js
+++ zira_clone/app/page.js
@@ -3,6 +3,19 @@ export default function Home() {
   return (
     <>
       Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
+      Hello World
     </>
   )
 }
