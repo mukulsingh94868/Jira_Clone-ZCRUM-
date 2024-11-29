ba1236496b182f36b1fc5a95ed642aa4b6169df7
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-11-29 11:10:36 +0530

    shadcn UI components installed

--- zira_clone/app/page.js
+++ zira_clone/app/page.js
@@ -1,5 +1,8 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return <div>
+   <Button>Hello World</Button> 
+  </div>;
 }
+
