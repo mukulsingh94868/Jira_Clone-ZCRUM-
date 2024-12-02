2d279f6405060290ac77506c3b8e64a23b93a37a
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-12-02 16:09:43 +0530

    set layout of user authentication

--- zira_clone/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ zira_clone/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
+import { SignUp } from "@clerk/nextjs";
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+
+export default function Page() {
+  return <SignUp />;
 }
-
-export default SignUp
